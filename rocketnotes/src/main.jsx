38b83a865c95b78896bc8e83d8e31fddd339ae42
alramosf <106263609+alramosf@users.noreply.github.com>
2023-07-02 21:57:00 -0300

    começando react

---
+++ rocketnotes/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import { Details } from './pages/Details'
+
+import theme from './styles/theme'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export function Details(){
+
+  return(
+    <h1>Hello World</h1>
+    
+  )
+}
+

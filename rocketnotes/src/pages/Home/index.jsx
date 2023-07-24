2c2fe9cf97f4e22a781d64968864b06732921d9f
alramosf <106263609+alramosf@users.noreply.github.com>
2023-07-24 13:19:54 -0300

    update

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
-import { Header } from '../../components/Header'
+import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
+
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 
@@ -24,7 +27,16 @@ export function Home(){
                 <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
             </Search>
             <Content>
-
+                <Section title= "Minhas Notas">
+                    <Note data={{
+                        title: 'React', 
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'Rocket'},
+                        ]
+                    }}
+                    />
+                </Section>
             </Content>
             <NewNote>
                 <FiPlus/>
